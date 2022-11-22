8108c2e583ae95b5656daa3a8f20ea20d6f9d1e9
nookkittipoj <n.kittipoj@gmail.com>
2022-11-22 11:10:06 +0700

    Update app.ts
    test

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import express, { Express, Request, Response } from 'express'
 export const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server (v1.0.0)')
+  res.send('Express + TypeScript Server (v1.0.0) ')
 })
