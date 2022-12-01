99c588fe813b101d3154b0708d9296afea46b147
Roy <pawit2540@hotmail.com>
2022-12-01 13:49:36 +0700

    test resource

--- terraform.tf
+++ terraform.tf
@@ -7,3 +7,13 @@ terraform {
     }
   }
 }
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "r" {
+  value = random_string.random.result
+}
